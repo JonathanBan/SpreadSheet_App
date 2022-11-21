d8ac949480d7c9554905c6bfdb152766e7443ab9
JonathanBan <87916849+JonathanBan@users.noreply.github.com>
2022-11-20 21:04:30 -0800

    Add files via upload

---
+++ Spreadsheet_Jonathan_Banos/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Spreadsheet_Jonathan_Banos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeBackroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoBackgroundColorChangeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redoTextChangeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-3, 36);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(904, 571);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 529);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Perform Demo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(900, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeBackroundColorToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(56, 29);
+            this.toolStripMenuItem1.Text = "Cell";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // changeBackroundColorToolStripMenuItem
+            // 
+            this.changeBackroundColorToolStripMenuItem.Name = "changeBackroundColorToolStripMenuItem";
+            this.changeBackroundColorToolStripMenuItem.Size = new System.Drawing.Size(323, 34);
+            this.changeBackroundColorToolStripMenuItem.Text = "Change Backround Color...";
+            this.changeBackroundColorToolStripMenuItem.Click += new System.EventHandler(this.changeBackroundColorToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoBackgroundColorChangeToolStripMenuItem,
+            this.redoTextChangeToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(58, 29);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // undoBackgroundColorChangeToolStripMenuItem
+            // 
+            this.undoBackgroundColorChangeToolStripMenuItem.Name = "undoBackgroundColorChangeToolStripMenuItem";
+            this.undoBackgroundColorChangeToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.undoBackgroundColorChangeToolStripMenuItem.Text = "Undo ";
+            this.undoBackgroundColorChangeToolStripMenuItem.Click += new System.EventHandler(this.undoBackgroundColorChangeToolStripMenuItem_Click);
+            // 
+            // redoTextChangeToolStripMenuItem
+            // 
+            this.redoTextChangeToolStripMenuItem.Name = "redoTextChangeToolStripMenuItem";
+            this.redoTextChangeToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.redoTextChangeToolStripMenuItem.Text = "Redo ";
+            this.redoTextChangeToolStripMenuItem.Click += new System.EventHandler(this.redoTextChangeToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Spreadsheet Cpts 321";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem changeBackroundColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoBackgroundColorChangeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redoTextChangeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+    }
+}
+
---
+++ Spreadsheet_Jonathan_Banos/Form1.cs
@@ -0,0 +1,423 @@
+﻿// Jonathan Banos 11667134
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cpts321;
+using SpreadsheetEngine1;
+using System.IO;
+
+namespace Spreadsheet_Jonathan_Banos
+{
+    public partial class Form1 : Form
+    {
+        // Calls the spreadsheet class to make a spreadsheet.
+        private SpreadSheet spreadSheetMember;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // We assign the new spreadsheet with a row of 50 and column of 26.
+            spreadSheetMember = new SpreadSheet(50, 26);
+
+            // subscribes to the cellchanged.
+            spreadSheetMember.CellPropertyChanged += Cellchanged;
+            dataGridView1.CellBeginEdit += dataGridView1_CellBeginEdit;
+            dataGridView1.CellEndEdit += dataGridView1_CellEndEdit;
+            updatedEditMenu();
+        }
+
+        /// <summary>
+        /// This is the form load so everything within it loads once the form loads.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // we have to clear the rows and columns before anything so it wont interfere with the previous  form.
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+
+            // column index is the index for the column.
+            int columnIndex = 0;
+
+            // This is to convert the column into a string.
+            string columnString;
+
+            // for loop from A-Z that is generated to create the column header.
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                // this gets the i and turns the char to string and assigns it to columnstring.
+                columnString = i.ToString();
+
+                // this adds to the column.
+                dataGridView1.Columns.Add("column" + columnIndex, columnString);
+
+                // Adds to the column index.
+                columnIndex++;
+            }
+
+            // string for the row.
+            string rowString;
+
+            // row that starts at 1 because of the header
+            int tempRow = 1;
+
+            // we have to add the rows before because then it doesn't work
+            dataGridView1.Rows.Add(50);
+
+            // This for loop is to add the numbers to the row headers
+            for (int j = 0; j < 50; j++)
+            {
+                // turns the row to a string
+                rowString = tempRow.ToString();
+
+                // This assings a row to the current row
+                var row = dataGridView1.Rows[j];
+
+                // makes the row header to the rowstring
+                row.HeaderCell.Value = rowString;
+
+                // this goes to the next row
+                tempRow++;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// This is called when the demo button is pressed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // makes a random value
+            Random random = new Random();
+
+            // makes a for loop on the 50 random strings.
+            for (int i = 0; i < 50; i++)
+            {
+                // makes random values between 0-50 and 0-26 for the rows and columns
+                int randRow = random.Next(0, 50);
+                int randCoumn = random.Next(0, 26);
+
+                // makes a cell class and calls the spreadsheet with the getcell function
+                Cell presentCell = spreadSheetMember.GetCell(randRow, randCoumn);
+
+                // Makes the text of the cell to hello world
+                presentCell.Text = "Hello World!";
+            }
+
+            // adding for loop to add column B plus the number
+            for (int i = 0; i < 50; i++)
+            {
+                // we make the present cell by calling the cell class and we get the value in column 1
+                Cell presentCell = spreadSheetMember.GetCell(i, 1);
+
+                // This makes the text be this is cell B and the current cell
+                presentCell.Text = "This is cell B" + (i + 1);
+            }
+
+            // need to get the column A be the same as B
+            for (int i = 0; i < 50; i++)
+            {
+                // This it the present cell and gets the value from column 0 which would be A
+                Cell presetCell = spreadSheetMember.GetCell(i, 0);
+                presetCell.Text = "=B" + i;
+            }
+        }
+
+        /// <summary>
+        /// This function is to update the cell and changes the form to if cell is changed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cellchanged(object sender, PropertyChangedEventArgs e)
+        {
+            // would be just like what we declared in spreadsheet engine
+            // if the cell is changed it goes into the if statement
+            if ("CellChange" == e.PropertyName)
+            {
+                // calls the cell clase and makes the object sender as a cell
+                Cell updateCell = sender as Cell;
+
+                // if its not null it goes into the if statement
+                if (updateCell != null)
+                {
+                    // will find row and column
+                    int rowCell = updateCell.RowIndex;
+                    int columnCell = updateCell.ColumnIndex;
+
+                    // Changes the datagrid cell value
+                    dataGridView1.Rows[rowCell].Cells[columnCell].Value = updateCell.Value;
+                }
+            }
+            else if ("ColorChanged" == e.PropertyName)
+            {
+                Cell updateCell = sender as Cell;
+
+                if (updateCell != null)
+                {
+                    // will find row and column
+                    int rowCell = updateCell.RowIndex;
+                    int columnCell = updateCell.ColumnIndex;
+
+                    // got this from the assignment
+                    int intColor = (int)updateCell.BGColor;
+                    Color color = Color.FromArgb(intColor);
+
+                    // Changes the datagrid cell value
+                    dataGridView1.Rows[rowCell].Cells[columnCell].Style.BackColor = color;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Got most of this code from the microsoft documentation but this helps with when the cell starts to get edited.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            // We set the row and columns to e
+            int row = e.RowIndex;
+            int column = e.ColumnIndex;
+
+            // This gets the cell
+            Cell updateCell = spreadSheetMember.GetCell(row, column);
+            if (updateCell != null)
+            {
+                dataGridView1.Rows[row].Cells[column].Value = updateCell.Text;
+            }
+        }
+
+        /// <summary>
+        /// Got most of this code form the microsoft documentation but this helps when the cell isn't edited anymore.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            // Gets the row and column
+            int row = e.RowIndex;
+            int column = e.ColumnIndex;
+
+            // This gets the cell
+            Cell updateCell = spreadSheetMember.GetCell(row, column);
+
+            // This checks if the cell got edited
+            bool checkEdit = true;
+
+            // Made a resotring text command for the text change
+            RestoringText[] undoText = new RestoringText[1];
+
+            string lastText = updateCell.Text;
+
+            undoText[0] = new RestoringText(updateCell, lastText);
+
+            if (updateCell != null)
+            {
+                // Had to make an exception to see if the text is deleted
+                try
+                {
+                    // need to check if the text didn't change
+                    if (updateCell.Text == dataGridView1.Rows[row].Cells[column].Value.ToString())
+                    {
+                        checkEdit = false;
+                    }
+
+                    // updates the text in the cell
+                    updateCell.Text = dataGridView1.Rows[row].Cells[column].Value.ToString();
+                }
+                catch
+                {
+                    if (updateCell.Text == null) checkEdit = false;
+                    updateCell.Text = "";
+                }
+
+                // Updates the cell to be able to show the value
+                dataGridView1.Rows[row].Cells[column].Value = updateCell.Value;
+
+                if (checkEdit == true)
+                {
+                    spreadSheetMember.AddUndo(new MulitCommand(undoText, "cell text change"));
+                    updatedEditMenu();
+                }
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// This will update the color to something else, got most of this code from the microsoft documentation given
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void changeBackroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Got this from micorsoft documentation which instantiates a type colorDialog
+            ColorDialog colorDialog = new ColorDialog();
+
+            List<RestoringColor> undoColors = new List<RestoringColor>();
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Goes thorugh all cells
+                foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
+                {
+                    Cell updateCell = spreadSheetMember.GetCell(cell.RowIndex, cell.ColumnIndex);
+
+                    uint lastColor = updateCell.BGColor;
+
+                    if (lastColor == 0) lastColor = (uint)Color.White.ToArgb();
+
+                    updateCell.BGColor = (uint)colorDialog.Color.ToArgb();
+
+                    RestoringColor undoColor = new RestoringColor(updateCell, lastColor);
+                    undoColors.Add(undoColor);
+                }
+            }
+
+            spreadSheetMember.AddUndo(new MulitCommand(undoColors.ToArray(), "changing cell background color"));
+            updatedEditMenu();
+        }
+
+        /// <summary>
+        /// When they click the undo button it calls the undo.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void undoBackgroundColorChangeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            spreadSheetMember.Undo();
+            updatedEditMenu();
+        }
+
+        /// <summary>
+        /// When they click the redo it calls the redo.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void redoTextChangeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            spreadSheetMember.Redo();
+            updatedEditMenu();
+        }
+
+        /// <summary>
+        /// I made a function where it edits the menu items if its undo or redo.
+        /// </summary>
+        private void updatedEditMenu()
+        {
+            ToolStripMenuItem editMenuItems = menuStrip1.Items[0] as ToolStripMenuItem;
+
+            foreach (ToolStripMenuItem menuItem in editMenuItems.DropDownItems)
+            {
+                if (menuItem.Text.Substring(0, 4) == "Undo")
+                {
+                    menuItem.Enabled = !(spreadSheetMember.UndoIsEmpty);
+                    menuItem.Text = "Undo" + spreadSheetMember.UndoCommand;
+                }
+                else if (menuItem.Text.Substring(0, 4) == "Redo")
+                {
+                    menuItem.Enabled = !(spreadSheetMember.RedoIsEmpty);
+                    menuItem.Text = "Redo" + spreadSheetMember.RedoCommand;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This saves the file once the user has clikced the save button on the tool strip, got a lot of this from microsoft documentation.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // we instantiate a save file diagolog to be able to save a file
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // if the user persses the ok button it goes into statement
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // we intatiate everything, this was in microsoft documentation
+                Stream oufile = new FileStream(saveFileDialog.FileName, FileMode.Create, FileAccess.Write);
+
+                // we save the spreadsheet
+                spreadSheetMember.Save(oufile);
+
+                // then we dispose of everything in the outfile
+                oufile.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// This function is for the load button, if its clicked then it opens a file dialog.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // This instatiates a openfiledialog to get the file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // If the user presses the ok button it goes into the if statement
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // We clear the spreadsheet first
+                ClearSheet();
+
+                // We instantiate a file I got this from microsoft documentation
+                Stream infile = new FileStream(openFileDialog.FileName, FileMode.Open, FileAccess.Read);
+
+                // we call the load function in spreadsheet
+                spreadSheetMember.Load(infile);
+
+                // We dispose of the contents in the infile
+                infile.Dispose();
+
+                // we clear all of the stacks in redo and undo in spreadsheet
+                spreadSheetMember.ClearStacks();
+            }
+
+            // we call the update edit menu to update it
+            updatedEditMenu();
+        }
+
+        /// <summary>
+        /// This function goes through every cell in the spreadsheet and clears that current cell.
+        /// </summary>
+        private void ClearSheet()
+        {
+            // iterates thorugh the rows
+            for (int i = 0; i < spreadSheetMember.RowCount; i++)
+            {
+                // iterates through the columns
+                for (int j = 0; j < spreadSheetMember.ColumnCount; j++)
+                {
+                    // Gets whats on the current cell
+                    Cell updateCell = spreadSheetMember.GetCell(i, j);
+
+                    // clears the cell
+                    updateCell.Clear();
+                }
+            }
+        }
+    }
+}
